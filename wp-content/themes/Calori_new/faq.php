26a912abe14921c51e022adf5cbdf83a5dad26b3
Denpavlov00 <117602764+Denpavlov00@users.noreply.github.com>
2024-12-18 03:24:26 +0300

    Update faq.php

--- wp-content/themes/Calori_new/faq.php
+++ wp-content/themes/Calori_new/faq.php
@@ -405,7 +405,7 @@ Ensimmäisen tilauksen jälkeen päätät itse jos haluat jatkaa palvelun käytt
     <div class="main__dark">
     </div>
 
-    <section class="bigbanner timer mb0">
+   <!-- <section class="bigbanner timer mb0">
         <div class="timer-wrap">
             <div class="banner-title">
             Ehdi tehdä tilaus
@@ -441,7 +441,30 @@ Ensimmäisen tilauksen jälkeen päätät itse jos haluat jatkaa palvelun käytt
             <a href="<?php echo get_home_url(); ?>#order" class="btn green">Tee tilaus</a>
         </div>
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/banner11.png" alt="banner" class="bigbanner-bg abs">
-    </section>
+    </section>-->
+
+     <section class="bigbanner timer mb0">
+      <div class="timer-wrap newbanner">
+        <div class="banner-title newbanner-title">
+          Uusi vuosi, <span>uusi sinä</span>
+        </div>
+        <div class="banner-text newbanner-text">
+          Seuraava toimitus on <span>Perjantai 3.1.2024</span>. <br><br>
+          Tee tilaus ennen uutta vuotta, ja saat 50€ alennuksen tilauksestasi koodilla:
+        </div>
+
+        <button class="promo">
+          CALORI25
+        </button>
+        
+        <div class="banner-after-text">
+          Koodi voimassa 18.12-31.12.2024, minimitilaus 100€.
+        </div>
+      </div>
+      <img src="<?php echo get_template_directory_uri() ?>/assets/images/banner11.png" alt="banner" class="bigbanner-bg abs">
+     </section>
+
+    
    <section class="section grey ">
     <div class="container">
       <div class="section-wrapper">
